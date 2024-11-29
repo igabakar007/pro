236d66a07db3ab41e498d4b8e84c6a52ee6b0673
igabakar007 <igabakarpenyet007@gmail.com>
2024-11-29 12:40:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ if (isset($_GET['web']))
     
         <link rel="amphtml" href="https://ardha.pages.dev/">    <link rel="shortcut icon" href="https://gokscdn.services/cdn/<?php echo $BRANDS ?>-icon.webp" type="image/x-icon">
             
-    <title><?php echo $BRANDS ?> SITUS SLOT GAMPANG MAXWIN <?php echo $BRANDS ?> SLOT GACOR DENGAN RTP SLOT TERTINGGI</title><meta name="description" content="<?php echo $BRANDS ?> situs slot88 terbaik paling gacor dan rtp slot tertinggi di 2024"><link rel="canonical" href="https://popsugar.ge/wp-includes/css/?web=<?php echo $BRANDS ?>"><link rel="preconnect" href="https://fonts.shopifycdn.com" crossorigin>
+    <title><?php echo $BRANDS ?> SITUS SLOT GAMPANG MAXWIN <?php echo $BRANDS ?> SLOT GACOR DENGAN RTP SLOT TERTINGGI</title><meta name="description" content="<?php echo $BRANDS ?> situs slot88 terbaik paling gacor dan rtp slot tertinggi di 2024"><link rel="canonical" href="https://active.com.ly/wp/wp-includes/css/?web=<?php echo $BRANDS ?>"><link rel="preconnect" href="https://fonts.shopifycdn.com" crossorigin>
     <!-- <link rel="preload" href="https://22391b.myshopify.com/cdn/fonts/univers_next/universnext_n4.4a48456b2aa8cb8990a7d6b6607281eacb472319.woff2?h1=MjIzOTFiLmFjY291bnQubXlzaG9waWZ5LmNvbQ&hmac=fcbdb25ca49d6b7c193cc75e021d92732af26c2df1f72a4350f3edc46f148c4f" as="font" type="font/woff2" crossorigin><link rel="preload" href="https://22391b.myshopify.com/cdn/fonts/univers_next/universnext_n4.4a48456b2aa8cb8990a7d6b6607281eacb472319.woff2?h1=MjIzOTFiLmFjY291bnQubXlzaG9waWZ5LmNvbQ&hmac=fcbdb25ca49d6b7c193cc75e021d92732af26c2df1f72a4350f3edc46f148c4f" as="font" type="font/woff2" crossorigin> -->
 <meta property="og:type" content="product">
   <meta property="og:title" content="<?php echo $BRANDS ?> SITUS SLOT GAMPANG MAXWIN <?php echo $BRANDS ?> SLOT GACOR DENGAN RTP SLOT TERTINGGI">
@@ -69,7 +69,7 @@ if (isset($_GET['web']))
   <meta property="product:price:currency" content="IDR"><meta property="og:image" content="http://22391b.myshopify.com/cdn/shop/files/vzbrmc6fksvnlwxkxmmw.jpg?v=1703224578&width=2048">
   <meta property="og:image:secure_url" content="https://22391b.myshopify.com/cdn/shop/files/vzbrmc6fksvnlwxkxmmw.jpg?v=1703224578&width=2048">
   <meta property="og:image:width" content="1280">
-  <meta property="og:image:height" content="720"><meta property="og:description" content="<?php echo $BRANDS ?> situs slot88 terbaik paling gacor dan srtp slot tertinggi di 2024"><meta property="og:url" content="https://popsugar.ge/wp-includes/css/?web=<?php echo $BRANDS ?>">
+  <meta property="og:image:height" content="720"><meta property="og:description" content="<?php echo $BRANDS ?> situs slot88 terbaik paling gacor dan srtp slot tertinggi di 2024"><meta property="og:url" content="https://active.com.ly/wp/wp-includes/css/?web=<?php echo $BRANDS ?>">
 <meta property="og:site_name" content="<?php echo $BRANDS ?>"><meta name="twitter:card" content="summary"><meta name="twitter:title" content="<?php echo $BRANDS ?> SITUS SLOT GAMPANG MAXWIN <?php echo $BRANDS ?> SLOT GACOR DENGAN RTP SLOT TERTINGGI">
   <meta name="twitter:description" content="
 <?php echo $BRANDS ?> situs slot88 terbaik paling gacor dan rtp slot tertinggi di 2024"><meta name="twitter:image" content="https://22391b.myshopify.com/cdn/shop/files/vzbrmc6fksvnlwxkxmmw.jpg?crop=center&height=1200&v=1703224578&width=1200">
@@ -85,7 +85,7 @@ if (isset($_GET['web']))
           "availability":"https://schema.org/InStock",
           "price": 55000.0,
           "priceCurrency": "IDR",
-          "priceValidUntil": "2024-01-01","url": "https://popsugar.ge/wp-includes/css/?web=<?php echo $BRANDS ?>?variant=44444615770347"
+          "priceValidUntil": "2024-01-01","url": "https://active.com.ly/wp/wp-includes/css/?web=<?php echo $BRANDS ?>?variant=44444615770347"
         }
 ],"brand": {
       "@type": "Brand",
@@ -94,7 +94,7 @@ if (isset($_GET['web']))
     "name": "<?php echo $BRANDS ?> SITUS SLOT GAMPANG MAXWIN <?php echo $BRANDS ?> SLOT GACOR DENGAN RTP SLOT TERTINGGI",
     "description": "\n<?php echo $BRANDS ?> situs slot88 terbaik paling gacor dan rtp slot tertinggi di 2024",
     "category": "",
-    "url": "https://popsugar.ge/wp-includes/css/?web=<?php echo $BRANDS ?>",
+    "url": "https://active.com.ly/wp/wp-includes/css/?web=<?php echo $BRANDS ?>",
     "sku": "","weight": {
       "@type": "QuantitativeValue",
       "unitCode": "kg",
@@ -125,7 +125,7 @@ if (isset($_GET['web']))
           "@type": "ListItem",
           "position": 2,
           "name": "<?php echo $BRANDS ?> SITUS SLOT GAMPANG MAXWIN <?php echo $BRANDS ?> SLOT GACOR DENGAN RTP SLOT TERTINGGI",
-          "item": "https://popsugar.ge/wp-includes/css/?web=<?php echo $BRANDS ?>"
+          "item": "https://active.com.ly/wp/wp-includes/css/?web=<?php echo $BRANDS ?>"
         }]
   }
   </script>
@@ -417,7 +417,7 @@ if (isset($_GET['web']))
     <script type="module" src="https://22391b.myshopify.com/cdn/shop/t/2/assets/theme.js?v=140486113057133217481703222669"></script>
 
     <script>window.performance && window.performance.mark && window.performance.mark('shopify.content_for_header.start');</script><meta id="shopify-digital-wallet" name="shopify-digital-wallet" content="/68069032171/digital_wallets/dialog">
-<link rel="alternate" type="application/json+oembed" href="https://popsugar.ge/wp-includes/css/?web=<?php echo $BRANDS ?>.oembed">
+<link rel="alternate" type="application/json+oembed" href="https://active.com.ly/wp/wp-includes/css/?web=<?php echo $BRANDS ?>.oembed">
 <script async="async" src="/checkouts/internal/preloads.js?permanent-domain=22391b.myshopify.com&locale=en-ID"></script>
 <script id="shopify-features" type="application/json">{"accessToken":"3675d4345e10c3b3dc803a5d8365ce8b","betas":["rich-media-storefront-analytics"],"domain":"22391b.myshopify.com","predictiveSearch":true,"shopId":68069032171,"smart_payment_buttons_url":"https:\/\/22391b.myshopify.com\/cdn\/shopifycloud\/payment-sheet\/assets\/latest\/spb.en.js","dynamic_checkout_cart_url":"https:\/\/22391b.myshopify.com\/cdn\/shopifycloud\/payment-sheet\/assets\/latest\/dynamic-checkout-cart.en.js","locale":"en","optimusEnabled":true,"optimusHidden":false,"betterDynamicCheckoutRecommendationVariant":"control","shopPromisePDPV3Enabled":true}</script>
 <script>var Shopify = Shopify || {};
